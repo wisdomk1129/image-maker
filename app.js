937ba23aac2594ef7cf8aa1e05105a6c838e9970
wisdomk1129 <wisdomk1129@naver.com>
2023-01-20 00:22:27 +0900

    canvas done
    text/image 삽입, 이미지 저장, 리셋, 지우개

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
+const saveBtn = document.querySelector(".save-btn");
+const textInput = document.querySelector(".text");
+const fileInput = document.querySelector(".file");
 const modeBtn = document.querySelector(".mode-btn");
+const resetBtn = document.querySelector(".reset-btn");
+const eraseBtn = document.querySelector(".erase-btn");
 const lineWidth = document.querySelector(".line-width");
 const color = document.querySelector(".color-setting__input");
 const colorOptions = Array.from(document.getElementsByClassName("color-setting__btn"));
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d"); //자주 사용되므로 변수이름 짧게
-canvas.width = 800;
-canvas.height = 800;
+
+const COLOR_NAME = "white";
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
+ctx.lineCap = "round";
 let isDrawing = false; //mousedown의 true/false를 알려주는 변수
 let isFilling = false; //fillingMode의 true/false를 알려주는 변수
 
@@ -41,11 +52,11 @@ function setColor(event) {
 
 function canvasFill() {
     if (isFilling) { //isFilling = true일 때만 캔버스를 가득 채움
-        ctx.fillRect(0, 0, 800, 800);
+        ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     }
 }
 
-function modeChange() {
+function paintingModeChange() {
     if (isFilling) { //fillMode → drawMode
         isFilling = false;
         modeBtn.innerText = "Fill";
@@ -54,6 +65,45 @@ function modeChange() {
         isFilling = true;
         modeBtn.innerText = "Draw";
     }
+}
+
+function canvasReset() {
+    ctx.fillStyle = COLOR_NAME;
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function canvasErase() {
+    isFilling = false; //지우개 선 그리기 위해 fillMode off
+    modeBtn.innerText = "Fill";
+    ctx.strokeStyle = COLOR_NAME;
+}
+
+function onFileChange(event) {
+    const uploadedFile = event.target.files["0"]; //유저가 업로드한 file 정보 가져오기
+    const image = new Image(); //image 만들기
+    image.src = URL.createObjectURL(uploadedFile); //image url 설정
+    image.onload = function () { //image가 로딩되면 canvas에 image 삽입
+        ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        fileInput.value = null; //imgae 삽입 후 fileInput 초기화 → 새로운 image 받을 수 있게 함
+    }
+}
+
+function insertText(event) {
+    const typedText = textInput.value;
+    if (typedText !== "") {
+        ctx.save(); //ctx 설정 저장
+        ctx.font = "30px serif";
+        ctx.fillText(typedText, event.offsetX, event.offsetY);
+        ctx.restore(); //저장한 ctx 설정 다시 불러오기
+    }
+}
+
+function saveImage() {
+    const savedImageUrl = canvas.toDataURL(); //canvas의 이미지 저장. url 형태
+    const downloadLink = document.createElement("a"); //anchor 생성
+    downloadLink.href = savedImageUrl; //anchor에 저장된 canvas 이미지 url 집어넣기
+    downloadLink.download = "myDrawing"; //download 기능 활성, 기본 이미지 이름 설정
+    downloadLink.click(); //자동으로 click
 }
 
 canvas.addEventListener("mousemove", canvasDraw);
@@ -66,5 +116,13 @@ lineWidth.addEventListener("change", setLineWidth);
 color.addEventListener("change", setColor);
 colorOptions.forEach(color => color.addEventListener("click", setColor));
 
-modeBtn.addEventListener("click", modeChange);
+modeBtn.addEventListener("click", paintingModeChange);
 canvas.addEventListener("click", canvasFill);
+
+resetBtn.addEventListener("click", canvasReset);
+eraseBtn.addEventListener("click", canvasErase);
+fileInput.addEventListener("change", onFileChange);
+
+canvas.addEventListener("dblclick", insertText);
+
+saveBtn.addEventListener("click", saveImage);
